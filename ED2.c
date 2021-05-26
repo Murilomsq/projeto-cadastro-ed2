ee63aac19d3478b83eb22f17e5e4a3b5ea3a4e5a
Murilo <murilol123@hotmail.com>
2021-05-25 21:56:54 -0300

    Header, Code cleanup, Bug fixing and func added
    Fixed bugs involving \n in strings, added a project header file with
    instructions to compile the code, added some comments separating the
    methods by utility,changed the way some methods works so they dont do
    too much,last but not least, added functionality that sorts the text
    file when the username is changed

--- ED2.c
+++ ED2.c
@@ -1,3 +1,18 @@
+//UFG
+//Engenharia de Computação - Estrutura de dados 2
+
+//Contribuidores:
+//Murilo Mesquita Carolina
+//Caio Roberto Duarte
+
+//CRUD simples onde cadastros são manipulados e armazenados em um arquivo txt, a senha é criptografada (operador XOR) pra ser armazenada, dessa forma
+//se acontece algum vazamento de banco de dados, existe uma barreira (mesmo que fraca, nesse caso) pra quem tem acesso ao arquivo, pois ele não armazena a senha
+//da forma que o usuario fornece.
+
+//Compilar esse arquivo com um arquivo de texto, no mesmo diretorio, com o nome "Usuarios", pra prevenir bugs e erros
+
+//
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,7 +20,9 @@
 #include <ctype.h>
 #include <stdbool.h>
 
-void encrypt(char *array, int array_size)
+#define FILE_NAME "Usuarios"
+
+void EncryptXOR(char *array, int array_size)
 {
     int i, j = 0;
     char secret[8] = {22, 53, 44, 71, 66, 177, 253, 122};
@@ -32,6 +49,172 @@ bool StringHasSpace(char *stringToCheck)
         }
     }
     return false;
+}
+
+//Métodos de leitura dos arquivos txt
+
+int CheckForUserPassword(char *fname, char *str, char *consoleMessage)
+{
+
+    char jumpLine[2] = "\n";
+    FILE *fp;
+    int line_num = 1;
+    int find_result = 0;
+    char temp[512];
+    char password[50];
+    int userFound = 0;
+    strcat(str, jumpLine);
+
+    if ((fp = fopen(fname, "r")) == NULL)
+    {
+        return -1;
+    }
+
+    //Pesquisa o usuario
+    while (fgets(temp, 512, fp) != NULL)
+    {
+
+        if (strcmp(str, temp) == 0)
+        {
+            system("cls");
+            printf("Usuario encontrado\nSenha: ");
+
+            scanf("%s", password);
+            userFound = 1;
+            break;
+        }
+        line_num++;
+    }
+    if (userFound == 0)
+    {
+        system("cls");
+        printf("User not found!");
+        return 1;
+    }
+
+    fclose(fp);
+    fp = fopen(fname, "r");
+
+    //Dá fgets até chegar na linha da senha do usuario
+    for (int i = 0; i < line_num + 1; i++)
+    {
+        fgets(temp, 512, fp);
+    }
+
+    //Criptografa a senha
+    EncryptXOR(password, strlen(password));
+    strcat(password, jumpLine);
+
+    fclose(fp);
+
+    //Checa se a senha criptografada é a correta
+    if ((strcmp(password, temp)) == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
+int GetNumberOfUsers(char *fName)
+{
+
+    FILE *fp;
+    int numOfUsers = 0;
+    int nextLineToGet = 1, actualLine = 1;
+    char buffer[50];
+
+    fp = fopen(fName, "r");
+    while (fgets(buffer, 50, fp) != NULL)
+    {
+        //Soma um usuario a cada 3 linhas
+        if ((actualLine + 3) % 3 == 1)
+        {
+            numOfUsers++;
+        }
+        actualLine++;
+    }
+    fclose(fp);
+    return numOfUsers;
+}
+
+int UserAtLine(char *fName, char *userName)
+{
+    char jumpLine[2] = "\n";
+    FILE *fp;
+    int line_num = 1;
+    int find_result = 0;
+    char temp[512];
+
+    if ((fp = fopen(fName, "r")) == NULL)
+    {
+        printf("No file found!");
+        return -1;
+    }
+
+    //Procura a String no arquivo
+    while (fgets(temp, 512, fp) != NULL)
+    {
+        if ((strcmp(temp, userName)) == NULL)
+        {
+            break;
+        }
+        line_num++;
+    }
+    fclose(fp);
+    return line_num;
+}
+
+int SearchInFile(char *fname, char *str)
+{
+    char jumpLine[2] = "\n";
+    FILE *fp;
+    int line_num = 1;
+    int find_result = 0;
+    char temp[512];
+
+    if ((fp = fopen(fname, "r")) == NULL)
+    {
+        printf("No file found!");
+        return -1;
+    }
+
+    //Cria uma copia para que o jumpline nao mude a string original parea checagem
+    char *strCopy = (char *)malloc(sizeof(char) * strlen(str) + 1);
+    strcpy(strCopy, str);
+
+    strcat(strCopy, jumpLine);
+
+    //Procura a String no arquivo
+    while (fgets(temp, 512, fp) != NULL)
+    {
+        if ((strcmp(temp, strCopy)) == NULL)
+        {
+            find_result++;
+        }
+        line_num++;
+    }
+
+    if (find_result == 0)
+    {
+        fclose(fp);
+        return -1;
+    }
+    free(strCopy);
+    fclose(fp);
+    return 0;
+}
+
+//Métodos de alteração do arquivo txt
+
+int PrintInANewLine(char *name, char *str)
+{
+    FILE *fp;
+    fp = fopen(name, "a+");
+    fputs(str, fp);
+    fclose(fp);
 }
 
 int DeleteUser(char *fName, int line)
@@ -70,28 +253,6 @@ int DeleteUser(char *fName, int line)
     rename("replace.txt", fName);
 
     return 0;
-}
-
-int GetNumberOfUsers(char *fName)
-{
-
-    FILE *fp;
-    int numOfUsers = 0;
-    int nextLineToGet = 1, actualLine = 1;
-    char buffer[50];
-
-    fp = fopen(fName, "r");
-    while (fgets(buffer, 50, fp) != NULL)
-    {
-        //Soma um usuario a cada 3 linhas
-        if ((actualLine + 3) % 3 == 1)
-        {
-            numOfUsers++;
-        }
-        actualLine++;
-    }
-    fclose(fp);
-    return numOfUsers;
 }
 
 int SortingTextFile(char *fname)
@@ -215,127 +376,13 @@ void ReplaceLineWith(int line, char *fName, char *textToReplace)
     rename("replace.txt", fName);
 }
 
-int CheckForUserPassword(char *fname, char *str, char *consoleMessage, int *line)
-{
-
-    char jumpLine[2] = "\n";
-    FILE *fp;
-    int line_num = 1;
-    int find_result = 0;
-    char temp[512];
-    char password[50];
-    int userFound = 0;
-    strcat(str, jumpLine);
-
-    if ((fp = fopen(fname, "r")) == NULL)
-    {
-        return -1;
-    }
-
-    //Pesquisa o usuario
-    while (fgets(temp, 512, fp) != NULL)
-    {
-
-        if (strcmp(str, temp) == 0)
-        {
-            system("cls");
-            *line = line_num;
-            printf("Usuario encontrado\nSenha: ");
-
-            scanf("%s", password);
-            userFound = 1;
-            break;
-        }
-        line_num++;
-    }
-    if (userFound == 0)
-    {
-        system("cls");
-        printf("User not found!");
-        return 1;
-    }
-
-    fclose(fp);
-    fp = fopen(fname, "r");
-
-
-    //Dá fgets até chegar na linha da senha do usuario
-    for (int i = 0; i < line_num + 1; i++)
-    {
-        fgets(temp, 512, fp);
-    }
-
-    //Criptografa a senha 
-    encrypt(password, strlen(password));
-    strcat(password, jumpLine);
-
-    fclose(fp);
-
-    //Checa se a senha criptografada é a correta
-    if ((strcmp(password, temp)) == 0)
-    {
-        return 0;
-    }
-    else
-    {
-        return 1;
-    }
-}
-
-int Search_in_File(char *fname, char *str)
-{
-    char jumpLine[2] = "\n";
-    FILE *fp;
-    int line_num = 1;
-    int find_result = 0;
-    char temp[512];
-
-    if ((fp = fopen(fname, "r")) == NULL)
-    {
-        printf("No file found!");
-        return -1;
-    }
-
-    //Cria uma copia para que o jumpline nao mude a string original parea checagem
-    char *strCopy = (char *)malloc(sizeof(char) * strlen(str) + 1);
-    strcpy(strCopy, str);
-
-    strcat(strCopy, jumpLine);
-
-    //Procura a String no arquivo
-    while (fgets(temp, 512, fp) != NULL)
-    {
-        if ((strcmp(temp, strCopy)) == NULL)
-        {
-            find_result++;
-        }
-        line_num++;
-    }
-
-    
-    if (find_result == 0)
-    {
-        fclose(fp);
-        return -1;
-    }
-    free(strCopy);
-    fclose(fp);
-    return 0;
-}
-
-int PrintInANewLine(char *name, char *str)
-{
-    FILE *fp;
-    fp = fopen(name, "a+");
-    fputs(str, fp);
-    fclose(fp);
-}
-
 int main()
 {
+
     system("cls");
+
     char consoleMessage[40] = "";
-    char state[20] = "logging";
+    int state = 1;
     int lookingAtLine = 0;
     while (1)
     {
@@ -343,31 +390,31 @@ int main()
         int option = 0;
         char userName[100];
         char jumpLine[2] = "\n";
-        char toEncrypt[200];
+        char toEncryptXOR[200];
         printf("%s\n\n", consoleMessage);
 
-        if (strstr(state, "logging"))
+        switch (state)
         {
-
-            printf("Deseja fazer login ou cadastrar-se?\nLogin (1)\nCadastro (2)\n");
+        case 1:
+            printf("Deseja fazer login ou cadastrar-se?\nLogin (1)\nCadastro (2)\nFinalizar Programa (3)\n");
             do
             {
                 scanf("%d", &option);
 
-                if (option != 1 && option != 2)
+                if (option != 1 && option != 2 && option != 3)
                 {
                     system("cls");
                     printf("Opcao invalida\n\n");
-                    printf("Deseja fazer login ou cadastrar-se?\nLogin (1)\nCadastro (2)\n");
+                    printf("Deseja fazer login ou cadastrar-se?\nLogin (1)\nCadastro (2)\nFinalizar Programa (3)\n");
                 }
 
                 while (getchar() != '\n')
                 {
                     system("cls");
                     printf("Opcao invalida\n\n");
-                    printf("Deseja fazer login ou cadastrar-se?\nLogin (1)\nCadastro (2)\n");
+                    printf("Deseja fazer login ou cadastrar-se?\nLogin (1)\nCadastro (2)\nFinalizar Programa (3)\n");
                 }
-            } while (option != 1 && option != 2);
+            } while (option != 1 && option != 2 && option != 3);
 
             switch (option)
             {
@@ -376,10 +423,12 @@ int main()
                 system("cls");
                 printf("Digite o nome de usuario: ");
                 scanf("%s", userName);
-                if (CheckForUserPassword("Usuarios", userName, consoleMessage, &lookingAtLine) == 0)
+                if (CheckForUserPassword(FILE_NAME, userName, consoleMessage) == 0)
                 {
+                    lookingAtLine = UserAtLine(FILE_NAME, userName);
+
                     strcpy(consoleMessage, "Logado com sucesso");
-                    strcpy(state, "logged");
+                    state = 2;
                     system("cls");
                 }
                 else
@@ -402,7 +451,7 @@ int main()
                     system("cls");
                     continue;
                 }
-                if (Search_in_File("Usuarios", userName) == 0)
+                if (SearchInFile(FILE_NAME, userName) == 0)
                 {
                     strcpy(consoleMessage, "Usuario ja existe");
 
@@ -410,30 +459,33 @@ int main()
                     continue;
                 }
                 printf("Digite sua senha: ");
-                scanf(" %[^\n]", toEncrypt);
-                if (StringHasSpace(toEncrypt))
+                scanf(" %[^\n]", toEncryptXOR);
+                if (StringHasSpace(toEncryptXOR))
                 {
                     strcpy(consoleMessage, "Senha nao pode ter espaco");
                     system("cls");
                     continue;
                 }
 
-                encrypt(toEncrypt, strlen(toEncrypt));
+                EncryptXOR(toEncryptXOR, strlen(toEncryptXOR));
 
-                strcat(toEncrypt, jumpLine);
+                strcat(toEncryptXOR, jumpLine);
                 strcat(userName, jumpLine);
-                PrintInANewLine("Usuarios", userName);
-                PrintInANewLine("Usuarios", toEncrypt);
-                PrintInANewLine("Usuarios", jumpLine);
+                PrintInANewLine(FILE_NAME, userName);
+                PrintInANewLine(FILE_NAME, toEncryptXOR);
+                PrintInANewLine(FILE_NAME, jumpLine);
 
-                SortingTextFile("Usuarios");
+                SortingTextFile(FILE_NAME);
                 strcpy(consoleMessage, "Cadastro concluido!");
                 system("cls");
                 break;
+
+            case 3:
+                system("cls");
+                return 0;
             }
-        }
-        else if (strstr(state, "logged"))
-        {
+            break;
+        case 2:
             printf("Usuario: %s\n", userName);
             printf("Mudar senha (1)\nMudar nome de usuario (2)\nDeslogar (3)\nDeletar usuario (4)\n");
             do
@@ -461,9 +513,17 @@ int main()
                 system("cls");
                 printf("Qual sera sua nova senha?\n");
                 char newPass[50];
-                scanf("%s", newPass);
-                encrypt(newPass, strlen(newPass));
-                ReplaceLineWith(lookingAtLine + 1, "Usuarios", newPass);
+
+                scanf(" %[^\n]", newPass);
+                if (StringHasSpace(newPass))
+                {
+                    strcpy(consoleMessage, "Senha nao pode ter espaco");
+                    system("cls");
+                    continue;
+                }
+
+                EncryptXOR(newPass, strlen(newPass));
+                ReplaceLineWith(lookingAtLine + 1, FILE_NAME, newPass);
                 strcpy(consoleMessage, "Senha alterada com sucesso!");
                 system("cls");
                 break;
@@ -472,7 +532,7 @@ int main()
                 printf("Qual sera sua novo nome de usuario?\n");
                 char newUser[50];
                 scanf("%s", newUser);
-                if (Search_in_File("Usuarios", newUser) == 0)
+                if (SearchInFile(FILE_NAME, newUser) == 0)
                 {
                     strcpy(consoleMessage, "Usuario ja existe");
 
@@ -480,28 +540,33 @@ int main()
                     continue;
                 }
 
-                ReplaceLineWith(lookingAtLine, "Usuarios", newUser);
+                ReplaceLineWith(lookingAtLine, FILE_NAME, newUser);
                 strcpy(userName, newUser);
                 strcat(userName, jumpLine);
                 strcpy(consoleMessage, "Nome de usuario alterado com sucesso");
+
+                SortingTextFile(FILE_NAME);
+
                 system("cls");
+                lookingAtLine = UserAtLine(FILE_NAME, userName);
+
                 break;
             case 3:
                 lookingAtLine = 0;
-                strcpy(state, "logging");
+                state = 1;
                 strcpy(consoleMessage, "Deslogado com sucesso");
                 system("cls");
                 break;
             case 4:
-                DeleteUser("Usuarios", lookingAtLine);
+                DeleteUser(FILE_NAME, lookingAtLine);
                 lookingAtLine = 0;
-                strcpy(state, "logging");
+                state = 1;
                 strcpy(consoleMessage, "Usuario deletado com sucesso");
                 system("cls");
                 break;
             }
+            break;
         }
     }
-
     return 0;
 }
